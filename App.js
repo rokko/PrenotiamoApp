c6ba67c6260eaf568622f0b9bf01c359540697fc
rokko <rocco.caricola89@gmail.com>
2021-02-27 13:18:09 +0100

    ok signup

--- App.js
+++ App.js
@@ -1,14 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import LoginScreen from './screens/LoginScreen'
+import SignupScreen from './screens/SignupScreen'
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 export default function App() {
   return (
     <>
       <SafeAreaProvider>
-        <LoginScreen />
+        <SignupScreen />
       </SafeAreaProvider>
     </>
   );
--- screens/SignupScreen.js
+++ screens/SignupScreen.js
@@ -1,48 +1,55 @@
 import React, { useState } from 'react'
-import { View, StyleSheet, TextInput } from 'react-native'
+import { View, StyleSheet, TextInput, ScrollView } from 'react-native'
 import Icon from 'react-native-vector-icons/FontAwesome';
-import { Input, Button } from 'react-native-elements';
+import { Input, Button, Text } from 'react-native-elements';
 import Alert from '../errors/Alert'
 
-export default function SignupScreen(){
-    <View style={{ flex: 1, padding: 10, backgroundColor: '#14213d', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-     <View style={{ backgroundColor: '#ffffff', height: 300, width: 300, borderRadius: '30%', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+export default function SignupScreen() {
+    const [mail, setMail] = useState('leocaricola@gmail.com')
+    const [password, setPassword] = useState('leolino11')
+    const [messageOpen, setMessageOpen] = useState(false)
+    const [error, setError] = useState('')
+    return (
+        <View style={{ flex: 1, padding: 10, backgroundColor: '#14213d', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+            <ScrollView style={{ backgroundColor: '#ffffff', marginTop: 100, height: 200, width: 300, borderTopRightRadius: '10%', borderTopLeftRadius: '10%', display: 'flex', }}>
                 <Input style={{ padding: 10, marginTop: 5 }}
                     leftIcon={{ type: 'font-awesome', name: 'user' }}
                     placeholder='Nome'
                     onChangeText={value => setMail(value)}
                 />
                 <Input style={{ padding: 10 }}
-                    leftIcon={{type:'font-awesome', name:'lock'}}
+                    leftIcon={{ type: 'font-awesome', name: 'user' }}
                     placeholder='Cognome'
                     onChangeText={value => setPassword(value)}
                 />
-                 <Input style={{ padding: 10 }}
-                    leftIcon={{type:'font-awesome', name:'lock'}}
+                <Input style={{ padding: 10 }}
+                    leftIcon={{ type: 'font-awesome', name: 'envelope' }}
                     placeholder='Mail'
                     onChangeText={value => setPassword(value)}
                 />
-                 <Input style={{ padding: 10 }}
-                    leftIcon={{type:'font-awesome', name:'lock'}}
+                <Input style={{ padding: 10 }}
+                    leftIcon={{ type: 'font-awesome', name: 'lock' }}
                     placeholder='Password'
                     onChangeText={value => setPassword(value)}
                 />
-                 <Input style={{ padding: 10 }}
-                    leftIcon={{type:'font-awesome', name:'lock'}}
+                <Input style={{ padding: 10 }}
+                    leftIcon={{ type: 'font-awesome', name: 'lock' }}
                     placeholder='Confema Password'
                     onChangeText={value => setPassword(value)}
                 />
-                 <Input style={{ padding: 10 }}
-                    leftIcon={{type:'font-awesome', name:'lock'}}
-                    placeholder='Confema Password'
+                <Input style={{ padding: 10 }}
+                    leftIcon={{ type: 'font-awesome', name: 'mobile' }}
+                    placeholder='Cellulare'
                     onChangeText={value => setPassword(value)}
                 />
+
                 
 
+            </ScrollView>
+            <Button style={{ width: 100 }} title='Registrati' onPress={() => submitlogin()}> {'Login'} </Button>
+                <Text style={{ marginTop: 6 }}> Non sei ancora registrato? Clicca qui</Text>
 
-                <Button style={{ width: 100 }} title='Login' onPress={() => submitlogin()}> {'Login'} </Button>
-                <Text style={{marginTop : 6}}> Non sei ancora registrato? Clicca qui</Text>
-            </View>
+
         </View>
-
+    )
 }
