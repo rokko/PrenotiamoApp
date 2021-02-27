7715404c48df851f1a65ae869341db32437be720
rokko <rocco.caricola89@gmail.com>
2021-02-27 19:59:16 +0100

    insert hook use form

--- screens/SignupScreen.js
+++ screens/SignupScreen.js
@@ -1,13 +1,10 @@
 import React, { useState } from 'react'
 import { View, ScrollView } from 'react-native'
 import { Input, Button, Text } from 'react-native-elements';
+import useForm from '../hooks/useForm'
 
 export default function SignupScreen() {
-    const [mail, setMail] = useState('leocaricola@gmail.com')
-    const [nome, setNome] = useState('')
-    const [password, setPassword] = useState('leolino11')
-    const [telefphonNumber,setTelephonNumber] = useState('')
-    const [cognome, setCognome] = useState('')
+    
     const [messageOpen, setMessageOpen] = useState(false)
     const [error, setError] = useState('')
 
@@ -30,36 +27,7 @@ export default function SignupScreen() {
     return (
         <View style={{ flex: 1, padding: 10, backgroundColor: '#14213d', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
             <ScrollView style={{ backgroundColor: '#ffffff', marginTop: 100, height: 200, width: 300, borderTopRightRadius: '10%', borderTopLeftRadius: '10%', display: 'flex', }}>
-                <Input style={{ padding: 10, marginTop: 5 }}
-                    leftIcon={{ type: 'font-awesome', name: 'user' }}
-                    placeholder='Nome'
-                    onChangeText={value => setNome(value)}
-                />
-                <Input style={{ padding: 10 }}
-                    leftIcon={{ type: 'font-awesome', name: 'user' }}
-                    placeholder='Cognome'
-                    onChangeText={value => setCognome(value)}
-                />
-                <Input style={{ padding: 10 }}
-                    leftIcon={{ type: 'font-awesome', name: 'envelope' }}
-                    placeholder='Mail'
-                    onChangeText={value => setMail(value)}
-                />
-                <Input style={{ padding: 10 }}
-                    leftIcon={{ type: 'font-awesome', name: 'lock' }}
-                    placeholder='Password'
-                    onChangeText={value => setPassword(value)}
-                />
-                <Input style={{ padding: 10 }}
-                    leftIcon={{ type: 'font-awesome', name: 'lock' }}
-                    placeholder='Confema Password'
-                    onChangeText={value => setPassword(value)}
-                />
-                <Input style={{ padding: 10 }}
-                    leftIcon={{ type: 'font-awesome', name: 'mobile' }}
-                    placeholder='Cellulare'
-                    onChangeText={value => setTelephonNumber(value)}
-                />
+               
 
                 
 
